ac6363caa978aa5f74d9c36cc8da503d8f809998
UDDISH-www <73113673+UDDISH-www@users.noreply.github.com>
2020-12-01 07:29:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var bullet,wall, thickness;
+var speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  bullet=createSprite(50,200,50,50);
+  bullet.shapeColor=color("white");
+
+  wall=createSprite(1500,200,thickness,height/2);
+  
+  speed=random(223,321)
+  weight=random(30,52)
+}
+
+function draw() {
+  background("black");
+  
+  wall.shapeColor=color(80,80,80);
+
+  bullet.velocityX = speed;
+
+  thickness=random(22,83);
+
+if(hasCollided(bullet,wall))
+{
+bullet.velocityX=0;
+var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+if(damage>10)
+{
+wall.shapeColor=color(255,0,0);
+}
+
+if(damage<10)
+{
+  wall.shapeColor=color(0,255,0);
+}
+}
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall)
+{
+bulletRightEdge=lbullet.x +lbullet.width
+wallLeftEdge=lwall.x 
+if (bulletRightEdge>=wallLeftEdge)
+{
+return true;
+}
+return false;
+}
